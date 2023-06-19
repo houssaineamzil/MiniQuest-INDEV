415a9c91e4b61e876f158346996475d56edfb3ce
harrybridgen <harrybridgen@aol.com>
2023-06-19 21:46:56 +0100

    updates

---
+++ source/tile/dungeon.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="dungeon" tilewidth="32" tileheight="32" tilecount="640" columns="32">
+ <image source="../img/dungeon.png" width="1024" height="640"/>
+</tileset>
