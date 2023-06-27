315240ddd1d9b0b0b09ca0a2437c34b5ad06cf59
harrybridgen <harrybridgen@aol.com>
2023-06-27 03:09:52 +0100

    g

--- source/miniquest.py
+++ source/miniquest.py
@@ -1,5 +1,5 @@
 from screenManager import ScreenManager
 
 if __name__ == "__main__":
-    manager = ScreenManager(700, 800, "source/tile/inn.tmx")
+    manager = ScreenManager(800, 800, "source/tile/inn.tmx")
     manager.run()
