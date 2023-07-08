0ed18d9e99609512030f2b582381322ab9754109
harrybridgen <harrybridgen@aol.com>
2023-07-08 23:37:21 +0100

    dsds

--- source/miniquest.py
+++ source/miniquest.py
@@ -4,8 +4,6 @@ from resourcePath import resource_path
 
 if __name__ == "__main__":
     manager = ScreenManager(1280, 720, "inn.tmx")
-
     icon = pygame.image.load(resource_path("source/img/mq.ico"))
     pygame.display.set_icon(icon)
-
     manager.run()
